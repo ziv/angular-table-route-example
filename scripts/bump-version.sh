301fc7c136ccd937ddae45ab2e56592a6f53498f
Ziv Perry <ziv.perry@gmail.com>
2025-07-30 14:51:13 +0300

    pages

---
+++ scripts/bump-version.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+LAST_TAG=$(git tag | tail -1)
+NEXT_VERSION=$(semver $LAST_TAG -i)
+
+#sed -i "s/UI_VERSION_PLACEHOLDER/v${NEXT_VERSION}/g" ./dist/techdebt/browser/config.json
+
+export NEXT_VERSION="v$NEXT_VERSION"
+echo "v$NEXT_VERSION"
+
